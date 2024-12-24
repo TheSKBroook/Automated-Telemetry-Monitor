c193aef3ca807abd59dcb09255991e8c2457440f
test <test@ubuntu.20.04.copy.myguest.virtualbox.org>
2024-12-24 16:47:02 +0800

    bugfix: add class structure in control.py. Also did some tests on converting the excel to rule files

--- temp/code/control.py
+++ temp/code/control.py
@@ -3,9 +3,32 @@
 from flask import Flask, request
 import ansible_runner, yaml, os, configparser
 
-inventory_file = os.path.abspath('inventory/inventory.ini')
+class Controller:
+    def __init__ (self, useful_data):
+        self.data = {**useful_data.pop('labels'), **useful_data.pop('annotations'), **useful_data}
+        
+    
+    def take_action(self):
+        if self.data.get("status") == "firing":
+            extravars = {}
+            r = ansible_runner.run(
+                private_data_dir = os.path.join(os.path.dirname(__file__), 'playbook'),
+                playbook='playbook.yml',
+                tags=self.data['alertname'],
+                extravars = {
+                    "target": targets.get(self.data['instance'].split(':')[0], None),
+                    **self.data
+                },
+                inventory=inventory_file
+                )
+        
+        elif self.data["status"] == "resolved":
+            ip = self.data["instance"].split(":")[0]
+            print(
+                "\nAlert ({}) from {} has been resolved.\n".format(self.data["alertname"], ip)
+            )
 
-app = Flask(__name__)
+inventory_file = os.path.abspath('inventory/inventory.ini')
 
 config = configparser.ConfigParser()
 config.read(inventory_file)
@@ -15,37 +38,20 @@ for target in config['targets']:
     ansible_host = config['targets'][target].split(' ')[0]
     targets[ansible_host] = target.split(' ')[0]
 
+app = Flask(__name__)
+
 @app.route("/webhookcallback", methods=["POST"])
 
 def hook():
+    
     yaml_data = yaml.safe_load(request.data.decode('utf-8'))
 
+    useful_data = {**yaml_data.get("alerts")[0]}
 
-    useful_data = {
-        **yaml_data.get("alerts")[0]
-    }
+    myController = Controller(useful_data)
 
-
-    if useful_data.get("status") == "firing":
-        extravars = {}
-        r = ansible_runner.run(
-            private_data_dir = os.path.join(os.path.dirname(__file__), 'playbook'),
-            playbook='playbook.yml',
-            tags=useful_data['labels']['alertname'],            
-            extravars = {
-                "COMPUTE_NODE": targets.get(useful_data['labels']['instance'].split(':')[0], None),
-                "PROCESS_NAME": useful_data['labels'].get('groupname', None)
-            },
-            inventory=inventory_file
-        )
-
-    elif useful_data.get("status") == "resolved":
-        ip = useful_data['labels']["instance"].split(":")[0]
-        print(
-            "\nAlert ({}) from {} has been resolved.\n".format(
-                useful_data["labels"].get("alertname"), ip
-            )
-        )
+    myController.take_action()
+    
     return request.data
 
 if __name__ == "__main__":
